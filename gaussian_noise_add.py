bac17f18b512aeb2b02054740bdafd37e433951e
BabaYaga840 <104863253+BabaYaga840@users.noreply.github.com>
2022-08-15 22:22:56 +0530

    Add files via upload

--- gaussian_noise_add.py
+++ gaussian_noise_add.py
@@ -5,20 +5,23 @@ from geometry_msgs.msg import Twist
 from numpy import random
 
 t=Twist()
-print("UwU")
+#print("UwU")
+g=random.normal(size=(2000,6))
+i=0
 def callback(data):
-	g=random.normal(size=(2,3))
-	t.linear.x=data.linear.x+g[0][0]
-	t.linear.y=data.linear.y+g[0][1]
-	t.linear.z=data.linear.z+g[0][2]
-	t.angular.x=data.angular.x+g[1][0]
-	t.angular.y=data.angular.y+g[1][1]
-	t.angular.z=data.angular.z+g[1][2]
-	
+	global i
+	t.linear.x=data.linear.x+gii][0]
+	t.linear.y=data.linear.y+gii][1]
+	t.linear.z=data.linear.z+g[i][2]
+	t.angular.x=data.angular.x+g[i][3]
+	t.angular.y=data.angular.y+g[i][4]
+	t.angular.z=data.angular.z+g[i][5]
+	i++
 	rospy.loginfo("subscriber.linear.x = %s", t.linear.x)
 	rospy.loginfo("subscriber.linear.y = %s", t.linear.y)
 	rospy.loginfo("subscriber.linear.z = %s", t.linear.z)
-	
+	if i=2000:
+		i=0
 def gauss():
 	pub=rospy.Publisher('noise_add', Twist, queue_size=10)
 	rospy.init_node('gaussian_noise_add', anonymous=True)
